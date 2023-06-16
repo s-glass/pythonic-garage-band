e017886412ef03453948ce1340860bf5534e7b05
SARAH GLASS <SGLASS87@GMAIL.COM>
2023-06-15 17:27:42 -0700

    I did that thing where I forgot to ACP throughout again, so I am committing everything this ACP.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# pythonic-garage-band
+# Lab - Class 04
+## Project: pythonic-garage-band
+
+Author: Sarah Glass for Python 401
+
+Collaborated with Dan Quinn, Logan Reece, and Anthony Sinitsa on the code.
+
+## Overview
+
+Create a Garage Band with Object Oriented Programming to include the following:
+
+* Use Python classes to model a Band made up of different kinds of musicians.
+
+* Start with Guitarist,Bassist, and Drummer.
+
+* Make use of a Musician base class to handle common functionality which particular kinds of musicians will inherit.
+
+## Links and Resources
+
+* Lots of TA and peer help.
+* Referenced class demo
+
+## Setup
+No .env requirements; gitignore invludes venv.
+
+## How to initialize/run your application
+
+* python3 band.py
+* python3 test_band.py
+
+## How to use your library
+No external libraries were brought in.
+
+## Tests
+All acceptance testing run through test_series.py file and pytest following TDD.
---
+++ pythonic_garage_band/band.py
@@ -0,0 +1,64 @@
+class Musician:
+
+    def __init__(self, name):
+        self.name = name
+
+class Band: 
+    instances = []
+    def __init__(self, name, members): ## Nirvana
+        self.name = name 
+        self.members = members
+        self.instances.append(self)
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return solos
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+
+
+class Guitarist(Musician):
+    def __init__(self, name):
+        self.name = name
+    def __str__ (self):
+        return f"My name is {self.name} and I play guitar"
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+    def get_instrument(self):
+        return 'guitar'
+    def play_solo(self):
+        return "face melting guitar solo"
+
+    
+
+class Bassist(Musician):
+    def __init__(self, name):
+        self.name = name
+    def __str__ (self):
+        return f"My name is {self.name} and I play bass"
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+    def get_instrument(self):
+        return 'bass'
+    def play_solo(self):
+        return "bom bom buh bom"
+
+class Drummer(Musician):
+    def __init__(self, name):
+        self.name = name
+    def __str__ (self):
+        return f"My name is {self.name} and I play drums"
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+    def get_instrument(self):
+        return 'drums'
+    def play_solo(self):
+        return "rattle boom crash"
+    
+
+# if __name__ == '__main__':
+#     # jedi1 = JediMaster("Yoda")
+#     # print(jedi1
